98791f9c512fdb24f5c21734253eef57f2dde9c5
username <Jzazzi56@gmail.comn>
2024-03-26 16:11:07 -0700

    running build load

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost/fantasy-store-api/api/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-    fs: {
-      strict: false,
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  optimizeDeps: {
-    exclude: ["path"],
-  },
 });
